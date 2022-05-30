31c2fefe5d1a635511446e4fc8b86fda32edc18b
bvdizon <bvdizon@gmail.com>
2022-05-31 06:14:43 +0800

    markup update on popup testimonial slider

---
+++ backup/agents.php
@@ -0,0 +1,46 @@
+<div class="breadcrumbs">
+    <ol>
+        <li><a href="/">Calgary Real Estate</a></li>
+        <li>The Group at RE/MAX First - REALTORS&reg;</li>
+    </ol>
+</div>
+<section style="margin-bottom: 5rem;">
+    <h1>Real Estate Agents in Calgary</h1>
+    <p><img class="lazyload" alt="Calgary Real Estate Team The Group" width="1242" height="698" data-src="/uploads/agent-1/the-group/team-photo-the-group-at-remax-first-v1.jpg" /></p>
+    <p>Hi, my name is Chris Audette, we are a process and tool based team of Calgary realtors&reg; called The Group at Remax First. We are one of the highest producing mega teams in Canada for 3 straight years. We&rsquo;ve helped over 1600 families successfully sell for top dollar and confidently buy up into the right new home. And we operate out of the number 1 Remax office in the world. We believe in giving back. For the last few years, I have been one of the top contributing individuals to the childrens miracle network in Canada (and so has my brokerage) I am proud to be able to make the claim, that each and every one of the agents on this team puts teh clients needs of the utmost importance and competently guides buyers and sellers through this complex process. But don't take my word for it, read the reviews below and talk directly with one of our agents today.</p>
+</section>
+<section style="margin-bottom: 5rem;">
+    <h2>The Group and The Community</h2>
+    <p><img class="pleft lazyload" alt="Childrens Miracle Network Miracle Agent" width="400" height="112" data-src="/uploads/agent-1/other-logos/childrens-miracle-network-thumb.jpg" />The Group at Re/Max First donates a portion of every transaction to the Alberta Children's Hospital through its charitable foundation The Children's Miracle Network. The team leader has been one of the top contributing individuals in Western Canada since starting this initiative.</p>
+</section>
+<section style="margin-bottom: 5rem;">
+    <h2>The Group at RE/MAX First - In the News</h2>
+    <ul class="columns" style="list-style: none; margin: 0 0 3rem 0; padding: 0; place-items: center;">
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="Avenue Magazine" width="120" height="45" data-src="/uploads/agent-1/other-logos/avenue-logo.jpg" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="Rep Canada" width="100" height="50" data-src="/uploads/agent-1/other-logos/REP-Canada-logo-v1.jpg" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="FOX logo" width="100" height="42" data-src="/uploads/agent-1/other-logos/fox-logo-v1.png" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="ESPN logo" width="150" height="45" data-src="/uploads/agent-1/other-logos/espn-logo.png" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="NBC logo" width="100" height="91" data-src="/uploads/agent-1/other-logos/nbc-logo.png" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="Real Estate Wealth" width="300" height="34" data-src="/uploads/agent-1/other-logos/olzz4c6pjuy9.jpg" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="MarketWatch logo" width="300" height="25" data-src="/uploads/agent-1/other-logos/marketwatch-logo-v1.png" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="CBS logo" width="200" height="47" data-src="/uploads/agent-1/other-logos/cbs-logo-v1.jpg" /></li>
+        <li class="column -width-1/5 -width-1/1@xs -width-1/2@sm -width-1/3@md"><img class="lazyload" alt="abc logo" width="100" height="96" data-src="/uploads/agent-1/other-logos/abc-logo.png" /></li>
+    </ul>
+</section>
+<section style="margin-bottom: 5rem;">
+    <h2>Real Estate Marketing Awards &amp; Recognition</h2>
+    <ul class="columns" style="list-style: none; margin: 0 0 3rem 0; padding: 0; place-items: center;">
+        <li style="margin-right: 2rem;"><img class="lazyload" alt="IAC Award" width="250" data-src="/uploads/agent-1/other-logos/iac_award-v1.jpg" /></li>
+        <li><img class="lazyload" alt="Titan Property Award" width="300" data-src="/uploads/agent-1/other-logos/TITAN-Property-Awards-Logo-v1.png" /></li>
+    </ul>
+</section>
+<section style="margin-bottom: 5rem;">
+    <div>#agents#</div>
+</section>
+<section>#the-group-google-reviews#</section>
+<section>
+    <h2>A History of Moving People - Your Area Experts</h2>
+    <h3>Over 1,600 transactions and nearly $700 Million in production experience</h3>
+    <p>The agents of The Group at Re/Max First have a combined sales of 1,613 transactions and $695,133,940.00 in total combined production, as of October 13, 2020.</p>
+    <div>#deals-by-the-group-agents-all-time#</div>
+</section>
