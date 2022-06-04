9eae078afdfb4e0c473da8df6fddd129a639800b
bvdizon <bvdizon@gmail.com>
2022-06-04 08:53:37 +0800

    completed the landing pages for realtors per area

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<!-- Header Part File -->
 <?php require 'template-parts/header.php'; ?>
+
 <!-- Entry Content -->
-<?php include 'content/realtors-expert-in-south-calgary-real-estate.php'; ?>
-<!-- End of Entry Content -->
+<?php include 'content/realtors-expert-in-mid-south-calgary-real-estate.php'; ?>
+
+<!-- To Load Scripts - Lazyload, etc -->
+<?php include 'content/styles-realtors-expert-in-cmty-real-estate.php'; ?>
+
+<!-- Footer Part File -->
 <?php require 'template-parts/footer.php'; ?>
